31f7400649797a9da75babd1c08c42f7c9392125
Ted M. Young <tedyoung@gmail.com>
2020-05-22 10:35:08 -0700

    Extracted WorldView to handle rendering a World to the canvas.
    Co-authored-by: Harald Reingruber <haraldreingruber@users.noreply.github.com>

---
+++ src/canvas/WorldView.ts
@@ -0,0 +1,32 @@
+import {World} from "../World";
+import {Player} from "../Player";
+import {PlayerShape} from "./PlayerShape";
+import {Wire} from "../Wire";
+import {WireShape} from "./WireShape";
+
+export class WorldView {
+  private readonly canvas: HTMLCanvasElement;
+  private readonly ctx: CanvasRenderingContext2D;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext('2d');
+
+    if (!this.ctx) {
+      throw new Error("Could not get Context2D");
+    }
+  }
+
+  public render(world: World) {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    let components = world.components();
+    for (let component of components) {
+      if (component instanceof Player) {
+        new PlayerShape(component).render(this.ctx);
+      } else if (component instanceof Wire) {
+        new WireShape(component).render(this.ctx);
+      }
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,28 @@
 import {World} from "./World";
 import {Player} from "./Player";
 import {Wire} from "./Wire";
-import {PlayerShape} from "./canvas/PlayerShape";
-import {WireShape} from "./canvas/WireShape";
+import {WorldView} from "./canvas/WorldView";
 
 const canvas = document.getElementById('world') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d');
 
-var world = new World();
-const player = new Player({id: '1'}, {x: 4, y: 10});
-const wire = new Wire({id: '2'}, player.id);
-wire.moveTo({x: 6, y: 10});
+function createWorld() {
+  const world = new World();
+  const player = new Player({id: '1'}, {x: 4, y: 10});
+  const wire = new Wire({id: '2'}, player.id);
+  wire.moveTo({x: 6, y: 10});
 
-world.add(player);
-world.add(wire);
+  world.add(player);
+  world.add(wire);
+  return world;
+}
+
+var world = createWorld();
 
 const nextTickButton = document.getElementById('nextTick') as HTMLButtonElement;
 nextTickButton.addEventListener('click', () => {
   world = world.nextCycle();
-  render(ctx, world);
+  worldView.render(world);
 });
 
-function render(canvasContext: CanvasRenderingContext2D, world: World) {
-  canvasContext.clearRect(0, 0, 600, 400);
-  let components = world.components();
-  for (let component of components) {
-    if (component instanceof Player) {
-      new PlayerShape(component).render(canvasContext);
-    } else if (component instanceof Wire) {
-      new WireShape(component).render(canvasContext);
-    }
-  }
-}
-
-if (ctx) {
-  render(ctx, world);
-}
+const worldView = new WorldView(canvas);
+worldView.render(world);
